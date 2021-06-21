abbad240f343ac14165eb5d15db304ea4f087db0
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-21 10:26:37 +0900

    고양이

---
+++ baekjoon/step-by-step/1/baek_10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//https://www.acmicpc.net/problem/10171
+int main()
+{
+    cout<<"\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+
+    return 0;
+}
