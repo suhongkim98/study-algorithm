b16e152531fadf1dc1c4384aeaa4eab7453f2509
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-21 10:27:44 +0900

    We love kriii

---
+++ baekjoon/step-by-step/1/baek_10718.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+//https://www.acmicpc.net/problem/10718
+int main()
+{
+    cout<<"강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+
+    return 0;
+}
