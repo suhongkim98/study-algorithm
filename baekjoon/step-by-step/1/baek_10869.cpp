bd1d772e456be1b69d7e68b5d5b22532c8701867
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-21 11:00:56 +0900

    Create baek_10869.cpp

---
+++ baekjoon/step-by-step/1/baek_10869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ //https://www.acmicpc.net/problem/10869
+int main(void)
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    cout << a-b <<endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+}
