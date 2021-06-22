62c555005afd9624790cf0bd0e22d7f2a0d7c879
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-22 14:43:14 +0900

    Create Baek11021.java

---
+++ baekjoon/step-by-step/3/Baek11021.java
@@ -0,0 +1,17 @@
+import java.util.*;
+//https://www.acmicpc.net/problem/11021
+public class Main{
+    
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        
+        for(int i = 1 ; i <= size ; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println("Case #" + i + ": " + (a+b));
+            
+        }
+        scanner.close();
+    }
+}
