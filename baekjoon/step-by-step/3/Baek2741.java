37fa2c98aa3f6550bf944ee9a4fcb301df6549cb
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-22 14:31:59 +0900

    Create Baek2741.java

---
+++ baekjoon/step-by-step/3/Baek2741.java
@@ -0,0 +1,13 @@
+import java.util.*;
+//https://www.acmicpc.net/problem/2741
+public class Main{
+    
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i = 1 ; i <= n ; i++) {
+            System.out.println(i);
+        }
+        scanner.close();
+    }
+}
