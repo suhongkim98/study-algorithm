c99e802a204fc30af18e0e85dfb189c3d83b64e2
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-23 21:42:58 +0900

    Create Baek15596.java

---
+++ baekjoon/step-by-step/5/Baek15596.java
@@ -0,0 +1,10 @@
+//https://www.acmicpc.net/problem/15596
+public class Test{
+    long sum(int[] a){
+        long ans = 0;
+        for(int i = 0 ; i < a.length ; i++) {
+            ans += a[i];
+        }
+        return ans;
+    }
+}
