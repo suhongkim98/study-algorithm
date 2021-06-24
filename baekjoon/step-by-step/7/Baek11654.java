3df88d42f8ba5dbbbae576115eb3f534ee73bcda
김수홍 <35598710+suhongkim98@users.noreply.github.com>
2021-06-24 15:14:09 +0900

    Create Baek11654.java

---
+++ baekjoon/step-by-step/7/Baek11654.java
@@ -0,0 +1,10 @@
+import java.util.*;
+//https://www.acmicpc.net/problem/11654
+public class Main{
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int ch = scanner.next().charAt(0);
+        System.out.println(String.format("%d", ch));
+        scanner.close();
+    }
+}
