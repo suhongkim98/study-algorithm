38520934d06ebdc13afb1b5fa77a0266725d1568
suhongkim98 <kshz9815@naver.com>
2022-02-01 00:24:29 +0900

    주석 수정

--- algorithm/search/UpperBoundLowerBound.java
+++ algorithm/search/UpperBoundLowerBound.java
@@ -1,6 +1,8 @@
 //이분탐색을 활용한 upper bound lower bound
-//이진 탐색은 정확히 같은 값을 찾는 것에 비해 Lower Bound는 주어진 인자 값보다 작거나 큰 값이 처음으로 나올 때 Return
-//Lower Bound와 마찬가지의 방식이지만 Upper Bound의 경우 인자 값보다 큰 첫번째 위치를 반환한다.
+//이진 탐색은 정확히 같은 값의 index를 찾는 것에 비해 
+//Lower Bound는 target 값보다 같거나 큰 값이 처음으로 나올 때 Return
+//Upper Bound의 경우 target 값보다 큰 값이 처음 나온 index를 반환한다.
+
 class UpperBoundLowerBound {
 	static int lowerBound(int[] array,  int value) {
         int low = 0;
